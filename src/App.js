c60bc03691804e0144d39fc46289f5a102f88e0e
EricFithian <ef2413@columbia.edu>
2021-06-17 12:31:33 -0400

    Added some login stuff

--- src/App.js
+++ src/App.js
@@ -4,21 +4,33 @@ import {GoogleLogin} from 'react-google-login';
 
 function App() {
   const responseGoogle = (response) => {
-    console.log(response);
-    console.log(response.profileObj);
+    console.log("My new response from google is" + response);
   }
 
-  return (
-    <div className="App">
-      <h1>Zach is the best student ever! :)</h1>
-      <GoogleLogin 
-      clientId='998838005929-3co4r9ejhf13l366vstrad3osu39e348.apps.googleusercontent.com'
-      buttonText="Zach wants you to login"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      />
-      </div>
-  );
+  console.log("This is my response from Google: " + responseGoogle());
+
+  if(responseGoogle !== undefined) {
+    return (
+      <div className="App">
+        <h1>Zach is the best student ever! :)</h1>
+        <h3>You are logged in! Yay!</h3>
+        <p>This is working</p>
+        </div>
+    ); 
+  }
+    else {
+      return (
+        <div>
+          <h1>You are not logged in sir!</h1>
+          <GoogleLogin 
+          clientId='998838005929-3co4r9ejhf13l366vstrad3osu39e348.apps.googleusercontent.com'
+          buttonText="Zach wants you to login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          />
+        </div>
+      )
+    }
 }
 
 export default App;
